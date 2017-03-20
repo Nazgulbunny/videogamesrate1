aeabf26a307f4d01b192e624ce6227eece5cc3cc
Paul Andrei <pbadarau@gmail.com>
2017-03-20 17:11:45 +0000

    Add AWS S3 bucket name

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.secrets.s3_secret_access_key, # required
       region:                'eu-central-1', # optional, defaults to 'us-east-1'
     }
-    config.fog_directory  = 'videogamesrate' # required
+    config.fog_directory  = 'videogamesrate.developement' # required
     config.fog_public     = true # optional, defaults to true
     config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
   end
