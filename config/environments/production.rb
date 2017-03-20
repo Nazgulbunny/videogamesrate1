dbe97c7853eb94c9d0f6b55cc1c5713cd21bf8c4
Paul Andrei <pbadarau@gmail.com>
2017-03-20 17:02:03 +0000

    Fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,8 +86,8 @@ Rails.application.configure do
     :address              => 'smtp.sendgrid.net',
     :port                 => '587',
     :domain               => 'heroku.com',
-    :user_name            => ENV['SENDGRID_PASSWORD'],
-    :password             => ENV['SENDGRID_USERNAME'],
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
