df0cef20227815a314626fd13afcb4eac0d6c612
Paul Andrei Badarau <pbadarau@gmail.com>
2017-03-28 17:59:33 +0200

    Add pn_auth_key to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327152027) do
+ActiveRecord::Schema.define(version: 20170328153038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -169,6 +169,7 @@ ActiveRecord::Schema.define(version: 20170327152027) do
     t.string   "phone_number"
     t.integer  "posts_count",            default: 0,      null: false
     t.string   "slug"
+    t.string   "pn_auth_key"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
