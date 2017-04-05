d670cdf5cda1f614cc5ed35669c9af8443f3796e
Paul Andrei Badarau <pbadarau@gmail.com>
2017-04-05 09:08:34 +0000

    Add the IP domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'http://videogamesrate.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://172.104.135.35' }
 
   # Paperclip defaults
   config.paperclip_defaults = {
@@ -96,7 +96,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :address              => 'mail.hover.com',
     :port                 => '587',
-    :domain               => 'videogamesrate.com',
+    :domain               => '172.104.135.35',
     :user_name            => ENV['SMTP_USERNAME'],
     :password             => ENV['SMTP_PASSWORD'],
     :authentication       => "plain",
