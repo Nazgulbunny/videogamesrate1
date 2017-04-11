8df52b62249cac6f3d1b3419592b324e19a433e8
Paul Andrei Badarau <pbadarau@gmail.com>
2017-04-11 19:14:00 +0200

    Update UI, clear old code, fix bugs, complete videos functionality

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,11 @@
 class Video < ActiveRecord::Base
   belongs_to :user
+
+  acts_as_votable
+  acts_as_commentable
+
+  include PublicActivity::Model
+  tracked only: [:create, :like], owner: Proc.new{ |controller, model| model.user }
+
 	mount_uploader :attachment, PostUploader
 end
