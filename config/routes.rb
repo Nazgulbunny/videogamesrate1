da2dc87b340a2f7588703547b9b4e150ec7d7d92
Paul Andrei Badarau <pbadarau@gmail.com>
2017-04-15 16:01:48 +0200

    Generate resource for games

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :games
   post '/rate' => 'rater#create', :as => 'rate'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
---
+++ spec/factories/games.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :game do
+    title "MyString"
+    release_date "2017-04-15 16:00:55"
+    developer "MyString"
+    publisher "MyString"
+    logo "MyString"
+    cover "MyString"
+    platform "MyString"
+  end
+end
