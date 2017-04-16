9fba95cced33128677b042e7317ee383d9c1d7fc
Paul Andrei Badarau <pbadarau@gmail.com>
2017-04-16 14:13:17 +0200

    Add index and show actions
    Add view templates
    Add rating
    Add extra links in the shared partial

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,15 @@
 class GamesController < ApplicationController
+  before_action :set_user
+  def index
+    @games = Game.order('title ASC')
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  private
+  def set_user
+    @user = current_user
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,9 @@
 class Game < ActiveRecord::Base
+  mount_uploader :logo, AvatarUploader
+  mount_uploader :cover, CoverUploader
+
+  acts_as_votable
+  acts_as_commentable
+  ratyrate_rateable "game"
+
 end
