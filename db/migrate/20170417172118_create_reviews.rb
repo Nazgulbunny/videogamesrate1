1dbb3e51a6170597ce569244e4089454d390297c
Paul Andrei Badarau <pbadarau@gmail.com>
2017-04-17 19:26:38 +0200

    Add user references

--- db/migrate/20170417172118_create_reviews.rb
+++ db/migrate/20170417172118_create_reviews.rb
@@ -3,6 +3,7 @@ class CreateReviews < ActiveRecord::Migration
     create_table :reviews do |t|
       t.integer :rating
       t.text :comment
+      t.references :user, index: true
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -194,9 +194,12 @@ ActiveRecord::Schema.define(version: 20170417172118) do
   create_table "reviews", force: :cascade do |t|
     t.integer  "rating"
     t.text     "comment"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username",               default: "",     null: false
