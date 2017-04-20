9063f3e5176a233dc1f0ffaece7e28d52ecc7197
Paul Andrei Badarau <pbadarau@gmail.com>
2017-04-20 16:27:37 +0200

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -16,260 +15,246 @@ ActiveRecord::Schema.define(version: 20170418215924) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_admin_comments", force: :cascade do |t|
-    t.string   "namespace"
-    t.text     "body"
-    t.string   "resource_id",   null: false
-    t.string   "resource_type", null: false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+  create_table "active_admin_comments", id: :serial, force: :cascade do |t|
+    t.string "namespace"
+    t.text "body"
+    t.string "resource_id", null: false
+    t.string "resource_type", null: false
+    t.integer "author_id"
+    t.string "author_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
+    t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
+    t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
-  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
-  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
-  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
-
-  create_table "activities", force: :cascade do |t|
-    t.integer  "trackable_id"
-    t.string   "trackable_type"
-    t.integer  "owner_id"
-    t.string   "owner_type"
-    t.string   "key"
-    t.text     "parameters"
-    t.integer  "recipient_id"
-    t.string   "recipient_type"
+  create_table "activities", id: :serial, force: :cascade do |t|
+    t.integer "trackable_id"
+    t.string "trackable_type"
+    t.integer "owner_id"
+    t.string "owner_type"
+    t.string "key"
+    t.text "parameters"
+    t.integer "recipient_id"
+    t.string "recipient_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type"
+    t.index ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type"
+    t.index ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type"
   end
 
-  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
-  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
-  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
-
-  create_table "admin_users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "admin_users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admin_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
-  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
-  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "average_caches", force: :cascade do |t|
-    t.integer  "rater_id"
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.float    "avg",           null: false
+  create_table "average_caches", id: :serial, force: :cascade do |t|
+    t.integer "rater_id"
+    t.integer "rateable_id"
+    t.string "rateable_type"
+    t.float "avg", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.string   "title",            limit: 50, default: ""
-    t.text     "comment"
-    t.integer  "commentable_id"
-    t.string   "commentable_type"
-    t.integer  "user_id"
-    t.string   "role",                        default: "comments"
+  create_table "comments", id: :serial, force: :cascade do |t|
+    t.string "title", limit: 50, default: ""
+    t.text "comment"
+    t.integer "commentable_id"
+    t.string "commentable_type"
+    t.integer "user_id"
+    t.string "role", default: "comments"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text     "comment_html"
+    t.text "comment_html"
+    t.index ["commentable_id"], name: "index_comments_on_commentable_id"
+    t.index ["commentable_type"], name: "index_comments_on_commentable_type"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id", using: :btree
-  add_index "comments", ["commentable_type"], name: "index_comments_on_commentable_type", using: :btree
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
-
-  create_table "events", force: :cascade do |t|
-    t.string   "name"
+  create_table "events", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.datetime "when"
-    t.integer  "user_id"
+    t.integer "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "cached_votes_up", default: 0
-    t.integer  "comments_count",  default: 0
+    t.integer "cached_votes_up", default: 0
+    t.integer "comments_count", default: 0
+    t.index ["cached_votes_up"], name: "index_events_on_cached_votes_up"
+    t.index ["comments_count"], name: "index_events_on_comments_count"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
-  add_index "events", ["cached_votes_up"], name: "index_events_on_cached_votes_up", using: :btree
-  add_index "events", ["comments_count"], name: "index_events_on_comments_count", using: :btree
-  add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
-
-  create_table "follows", force: :cascade do |t|
-    t.integer  "followable_id",                   null: false
-    t.string   "followable_type",                 null: false
-    t.integer  "follower_id",                     null: false
-    t.string   "follower_type",                   null: false
-    t.boolean  "blocked",         default: false, null: false
+  create_table "follows", id: :serial, force: :cascade do |t|
+    t.integer "followable_id", null: false
+    t.string "followable_type", null: false
+    t.integer "follower_id", null: false
+    t.string "follower_type", null: false
+    t.boolean "blocked", default: false, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["followable_id", "followable_type"], name: "fk_followables"
+    t.index ["follower_id", "follower_type"], name: "fk_follows"
   end
 
-  add_index "follows", ["followable_id", "followable_type"], name: "fk_followables", using: :btree
-  add_index "follows", ["follower_id", "follower_type"], name: "fk_follows", using: :btree
-
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string   "slug",                      null: false
-    t.integer  "sluggable_id",              null: false
-    t.string   "sluggable_type", limit: 50
-    t.string   "scope"
+  create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
     t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
-  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
-  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
-  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
-  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
-
-  create_table "games", force: :cascade do |t|
-    t.string   "title"
+  create_table "games", id: :serial, force: :cascade do |t|
+    t.string "title"
     t.datetime "release_date"
-    t.string   "developer"
-    t.string   "publisher"
-    t.string   "logo"
-    t.string   "cover"
-    t.string   "platform"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.string   "genre"
-    t.string   "webpage"
+    t.string "developer"
+    t.string "publisher"
+    t.string "logo"
+    t.string "cover"
+    t.string "platform"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "genre"
+    t.string "webpage"
   end
 
-  create_table "overall_averages", force: :cascade do |t|
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.float    "overall_avg",   null: false
+  create_table "overall_averages", id: :serial, force: :cascade do |t|
+    t.integer "rateable_id"
+    t.string "rateable_type"
+    t.float "overall_avg", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.text     "content",                     null: false
-    t.integer  "user_id"
-    t.string   "attachment"
+  create_table "posts", id: :serial, force: :cascade do |t|
+    t.text "content", null: false
+    t.integer "user_id"
+    t.string "attachment"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "cached_votes_up", default: 0
-    t.integer  "comments_count",  default: 0
-    t.text     "content_html"
+    t.integer "cached_votes_up", default: 0
+    t.integer "comments_count", default: 0
+    t.text "content_html"
+    t.index ["cached_votes_up"], name: "index_posts_on_cached_votes_up"
+    t.index ["comments_count"], name: "index_posts_on_comments_count"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  add_index "posts", ["cached_votes_up"], name: "index_posts_on_cached_votes_up", using: :btree
-  add_index "posts", ["comments_count"], name: "index_posts_on_comments_count", using: :btree
-  add_index "posts", ["user_id"], name: "index_posts_on_user_id", using: :btree
-
-  create_table "rates", force: :cascade do |t|
-    t.integer  "rater_id"
-    t.integer  "rateable_id"
-    t.string   "rateable_type"
-    t.float    "stars",         null: false
-    t.string   "dimension"
+  create_table "rates", id: :serial, force: :cascade do |t|
+    t.integer "rater_id"
+    t.integer "rateable_id"
+    t.string "rateable_type"
+    t.float "stars", null: false
+    t.string "dimension"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type"
+    t.index ["rater_id"], name: "index_rates_on_rater_id"
   end
 
-  add_index "rates", ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type", using: :btree
-  add_index "rates", ["rater_id"], name: "index_rates_on_rater_id", using: :btree
-
-  create_table "rating_caches", force: :cascade do |t|
-    t.integer  "cacheable_id"
-    t.string   "cacheable_type"
-    t.float    "avg",            null: false
-    t.integer  "qty",            null: false
-    t.string   "dimension"
+  create_table "rating_caches", id: :serial, force: :cascade do |t|
+    t.integer "cacheable_id"
+    t.string "cacheable_type"
+    t.float "avg", null: false
+    t.integer "qty", null: false
+    t.string "dimension"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
   end
 
-  add_index "rating_caches", ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type", using: :btree
-
-  create_table "reviews", force: :cascade do |t|
-    t.integer  "rating"
-    t.text     "comment"
-    t.string   "video_review"
-    t.integer  "user_id"
-    t.integer  "game_id"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.integer  "comments_count", default: 0
+  create_table "reviews", id: :serial, force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.string "video_review"
+    t.integer "user_id"
+    t.integer "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "comments_count", default: 0
+    t.index ["comments_count"], name: "index_reviews_on_comments_count"
+    t.index ["game_id"], name: "index_reviews_on_game_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
-  add_index "reviews", ["comments_count"], name: "index_reviews_on_comments_count", using: :btree
-  add_index "reviews", ["game_id"], name: "index_reviews_on_game_id", using: :btree
-  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
-
-  create_table "users", force: :cascade do |t|
-    t.string   "username",               default: "",     null: false
-    t.string   "email",                  default: "",     null: false
-    t.string   "encrypted_password",     default: "",     null: false
-    t.string   "about"
-    t.string   "avatar"
-    t.string   "cover"
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "username", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "about"
+    t.string "avatar"
+    t.string "cover"
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,      null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
-    t.string   "sex",                    default: "male", null: false
-    t.string   "location"
-    t.date     "dob"
-    t.string   "phone_number"
-    t.integer  "posts_count",            default: 0,      null: false
-    t.string   "slug"
-    t.string   "pn_auth_key"
-    t.boolean  "admin",                  default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "sex", default: "male", null: false
+    t.string "location"
+    t.date "dob"
+    t.string "phone_number"
+    t.integer "posts_count", default: 0, null: false
+    t.string "slug"
+    t.string "pn_auth_key"
+    t.boolean "admin", default: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["slug"], name: "index_users_on_slug", unique: true
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["slug"], name: "index_users_on_slug", unique: true, using: :btree
-
-  create_table "videos", force: :cascade do |t|
-    t.text     "description"
-    t.string   "attachment"
-    t.integer  "user_id"
+  create_table "videos", id: :serial, force: :cascade do |t|
+    t.text "description"
+    t.string "attachment"
+    t.integer "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "cached_votes_up", default: 0
-    t.integer  "comments_count",  default: 0
+    t.integer "cached_votes_up", default: 0
+    t.integer "comments_count", default: 0
+    t.index ["cached_votes_up"], name: "index_videos_on_cached_votes_up"
+    t.index ["comments_count"], name: "index_videos_on_comments_count"
+    t.index ["user_id"], name: "index_videos_on_user_id"
   end
 
-  add_index "videos", ["cached_votes_up"], name: "index_videos_on_cached_votes_up", using: :btree
-  add_index "videos", ["comments_count"], name: "index_videos_on_comments_count", using: :btree
-  add_index "videos", ["user_id"], name: "index_videos_on_user_id", using: :btree
-
-  create_table "votes", force: :cascade do |t|
-    t.integer  "votable_id"
-    t.string   "votable_type"
-    t.integer  "voter_id"
-    t.string   "voter_type"
-    t.boolean  "vote_flag"
-    t.string   "vote_scope"
-    t.integer  "vote_weight"
+  create_table "votes", id: :serial, force: :cascade do |t|
+    t.integer "votable_id"
+    t.string "votable_type"
+    t.integer "voter_id"
+    t.string "voter_type"
+    t.boolean "vote_flag"
+    t.string "vote_scope"
+    t.integer "vote_weight"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+    t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
   end
-
-  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
-  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
 
 end
