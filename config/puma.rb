11d26531ad83afccbcbeda20ea2861c70fe15247
Paul Andrei Badarau <pbadarau@gmail.com>
2017-04-20 16:41:54 +0200

    Re-do puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,13 +1,23 @@
 # Change to match your CPU core count
 workers 1
 
-# Min and Max threads per worker
-threads 1, 6
+# Puma can serve each request in a thread from an internal thread pool.
+# The `threads` method setting takes two numbers: a minimum and maximum.
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Default is set to 5 threads for minimum
+# and maximum; this matches the default thread size of Active Record.
+#
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
+
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+#
+port        ENV.fetch("PORT") { 3000 }
 
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 
-# Default to production
+# Specifies the `environment` that Puma will run in.
 rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
