6d30fd6e647e0f49d1813fb6ce710021ff069f3f
Paul Andrei Badarau <pbadarau@gmail.com>
2017-04-21 20:03:25 +0200

    Render only the current user's followers

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,11 +1,18 @@
 class ChatController < ApplicationController
+  before_action :set_user, except: :front
   before_action :authenticate_user!
-  
+
   def index
     session[:conversations] ||= []
 
-    @users = User.all.where.not(id: current_user)
+    @users = @user.following_users.paginate(page: params[:page])
     @conversations = Conversation.includes(:recipient, :messages)
                                  .find(session[:conversations])
   end
+
+  private
+  def set_user
+    @user = current_user
+  end
+
 end
