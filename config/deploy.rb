8aa019bf47c5463f50a059c99385e93e711bdf1b
Rystraum Gamonez <rystraum@gmail.com>
2017-09-29 14:58:26 +0800

    Fixed ansible deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,76 +1,84 @@
-# Change these
-server Figaro.env.PRODUCTION_SERVER_IP, roles: [:web, :app, :db], primary: true
+# config valid only for Capistrano 3.1
+lock '3.8.0'
 
-set :repo_url,        'git@github.com:Nazgulbunny/videogamesrate1.git'
-set :application,     'videogamesrate'
-set :user,            'deploy'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
+# switches that affect where to get the code
+set :application, 'vgr'
+set :repo_url, 'git@github.com:Nazgulbunny/videogamesrate1.git'
+set :keep_releases, 5
+set :pty, true
 
-# Don't change these unless you know what you're doing
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+# switches that affect bundler
+# remove capistrano-bundler settings that do gem isolation
+# since bundle config and rbenv-binstubs already do that
+set :bundle_binstubs, nil # do not use shared bundle path between releases
+set :bundle_path,     nil # trust bundle config to set the path
+set :bundle_flags,    nil # be verbose, don't use deployment mode
 
-## Defaults:
-# set :scm,           :git
-# set :branch,        :master
-# set :format,        :pretty
-# set :log_level,     :debug
-# set :keep_releases, 5
+# files / directories that are just symlinks to entries in shared
+set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets system public/system}
 
-## Linked Files & Directories (Default None):
-append :linked_files, "config/database.yml", "config/secrets.yml", "config/application.yml"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
+namespace :deploy do
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
-  before :start, :make_dirs
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
 end
 
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
+namespace :db do
+  task :seed do
     on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, "db:seed"
+        end
       end
     end
   end
+end
 
-  desc 'Initial Deploy'
-  task :initial do
+namespace :service do
+  task :restart do
     on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
+      execute :sudo, "service #{fetch :service} stop"
+      execute :sudo, "service #{fetch :service} start"
     end
   end
 
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
+  task :start do
+    on roles(:app) do
+      execute :sudo, "service #{fetch :service} start"
+    end
+  end
+
+  task :stop do
+    on roles(:app) do
+      execute :sudo, "service #{fetch :service} stop"
+    end
+  end
 end
 
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
+task :test do
+  on roles(:app) do
+    within release_path do
+      execute :ruby, "-v"
+      # execute :export
+    end
+  end
+end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,61 +1,27 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
+# stage settings
+set :stage, :production
+set :branch, 'master'
+set :user, 'vgr'
 
-# server "example.com", user: "deploy", roles: %w{app db web}
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
+server 'www.videogamesrate.net', user: 'vgr', roles: %w{web app db}
 
+set :ssh_options, {
+  keys: %w(~/.ssh/blank),
+  forward_agent: true
+}
 
+# deploy location
+set :full_app_name, "#{fetch(:application)}"
+set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:full_app_name)}"
 
-# role-based syntax
-# ==================
+# init service name
+set :service, 'www.videogamesrate.net'
 
-# Defines a role with one or multiple servers. The primary server in each
-# group is considered to be the first unless any hosts have the primary
-# property set. Specify the username and a domain or IP for the server.
-# Don't use `:all`, it's a meta role.
+# rails settings
+set :rails_env, 'production'
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+set :rbenv_path, "/home/#{fetch(:user)}/.rbenv"
+set :rbenv_type, :user
+set :rbenv_ruby, "2.3.1"
 
-
-
-# Configuration
-# =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
-
-
-
-# Custom SSH Options
-# ==================
-# You may pass any option but keep in mind that net/ssh understands a
-# limited set of options, consult the Net::SSH documentation.
-# http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start
-#
-# Global options
-# --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server "example.com",
-#   user: "user_name",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "user_name", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
+set :tmp_dir, "/home/#{fetch(:user)}/.capistrano-tmp"
